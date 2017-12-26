6ea6d2a5e42ab10ab9127e8d65819ea6d7e2eaea
James Hamann <jameshamann@me.com>
2017-12-26 12:45:44 +0200

    changes path name from external_feed to medium_posts

--- README.md
+++ README.md
@@ -63,4 +63,4 @@ Bug reports and pull requests are welcome on GitHub at https://github.com/jamesh
 
 ## License
 
-The theme is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
+The gem is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
--- lib/jekyll-display-medium-posts.rb
+++ lib/jekyll-display-medium-posts.rb
@@ -4,14 +4,14 @@ module Jekyll
     safe true
     priority :high
 def generate(site)
-      jekyll_coll = Jekyll::Collection.new(site, 'external_feed')
-      site.collections['external_feed'] = jekyll_coll
+      jekyll_coll = Jekyll::Collection.new(site, 'medium_posts')
+      site.collections['medium_posts'] = jekyll_coll
 Feedjira::Feed.fetch_and_parse("https://medium.com/feed/@" + ENV["MEDIUM_USERNAME"]).entries.each do |e|
         p "Title: #{e.title}, published on Medium #{e.url} #{e}"
         title = e[:title]
         content = e[:content]
         guid = e[:url]
-        path = "./_external_feed/" + title + ".md"
+        path = "./medium_posts/" + title + ".md"
         path = site.in_source_dir(path)
         doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
         doc.data['title'] = title;
