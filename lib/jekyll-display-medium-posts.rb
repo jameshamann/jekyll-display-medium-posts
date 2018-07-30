c09ade346968fbcdc41a656fc686385eaa425190
James Hamann <jameshamann@me.com>
2018-07-30 09:41:20 +0100

    adds and saves url for use in linking to blog post on medium

--- lib/jekyll-display-medium-posts.rb
+++ lib/jekyll-display-medium-posts.rb
@@ -17,6 +17,7 @@ Feedjira::Feed.fetch_and_parse("https://medium.com/feed/@" + ENV["MEDIUM_USERNAM
         doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
         doc.data['title'] = title;
         doc.data['feed_content'] = content;
+        doc.data['link'] = link;
         jekyll_coll.docs << doc
       end
     end
